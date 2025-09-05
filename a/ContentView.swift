fd303bae275a45b28ea0c0e00fed0af11c9163ec
G <doliy4784@gmail.com>
2025-09-05 15:10:54 +0800

    Initial Commit

---
+++ a/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  a
+//
+//  Created by 張庭瑄 on 2025/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ a/aApp.swift
@@ -0,0 +1,17 @@
+//
+//  aApp.swift
+//  a
+//
+//  Created by 張庭瑄 on 2025/9/5.
+//
+
+import SwiftUI
+
+@main
+struct aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
